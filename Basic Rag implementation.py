9d3008f72f671e1f9c0cdb398d765d9575cce02b
Ujwala Rekha <142649169+ujwala123rekha@users.noreply.github.com>
2026-03-06 23:48:44 +0530

    Update Basic Rag implementation.py

--- Basic Rag implementation.py
+++ Basic Rag implementation.py
@@ -14,5 +14,85 @@
        ↓
     LLM
        ↓
-    Answer"""
+    Answer
+    
+    I had used 2 models for embedding and llm model
 
+    nomic-embed-text   for embedding
+    gemma 3:4b    for model
+    """
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
+
+
+loader = PyPDFLoader(r"C:\Users\UJWALA\Downloads\present resume 1.pdf")
+documents = loader.load()
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+docs = text_splitter.split_documents(documents)
+
+
+embedding_model = OllamaEmbeddings(model="nomic-embed-text")
+
+vectorstore = FAISS.from_documents(docs, embedding_model)
+
+retriever = vectorstore.as_retriever(search_kwargs={"k":3})
+
+
+llm = ChatOllama(model="gemma3:4b")
+
+
+prompt_template = """
+Answer the question using the context below.
+If the answer is not present in the context, say "I don't know".
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+prompt = PromptTemplate(
+    template=prompt_template,
+    input_variables=["context", "question"]
+)
+
+
+def rag_pipeline(query):
+
+    retrieved_docs = retriever.invoke(query)
+
+    context = "\n".join([doc.page_content for doc in retrieved_docs])
+
+    final_prompt = prompt.format(
+        context=context,
+        question=query
+    )
+
+    response = llm.invoke(final_prompt)
+    return response.content
+
+
+while True:
+
+    query = input("\nAsk something (type 'exit' to quit): ")
+
+    if query.lower() == "exit":
+        break
+
+    answer = rag_pipeline(query)
+
+    print("\nAnswer:\n", answer)
